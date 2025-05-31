35aac870aa3c5d90c66482c2c95e09a3f6b1d785
Mohamed Ebrahim Eldesoky <mohamedebrahimeldesoky12@gmail.com>
2025-05-31 16:27:25 +0300

    Time: 9 ms (42.19%), Space: 55.9 MB (58.17%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
@@ -1,30 +1,30 @@
 public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) {
-       
+        // نرتب كل مصفوفة
+        Array.Sort(nums1);
+        Array.Sort(nums2);
 
-        int totalLength = nums1.Length+nums2.Length;
-        double [] mergedArr = new double[totalLength];
-        for(int i =0; i<nums1.Length ; i++)
-        {
-            mergedArr[i] = (double)nums1[i];
-        }
-        for(int x =0; x<nums2.Length; x++)
-            {
-                mergedArr[nums1.Length+x] = (double)nums2[x];
+        int m = nums1.Length, n = nums2.Length;
+        double[] mergedArr = new double[m + n];
+
+        // دمج مصفوفتين مرتبتين بطريقة مرتبة
+        int i = 0, j = 0, k = 0;
+        while (i < m && j < n) {
+            if (nums1[i] < nums2[j]) {
+                mergedArr[k++] = nums1[i++];
+            } else {
+                mergedArr[k++] = nums2[j++];
             }
-       
-        Array.Sort(mergedArr);
-
-
-        if (totalLength % 2 == 1)
-        {
-            return mergedArr[totalLength / 2];
         }
-        else
-        {
+
+        while (i < m) mergedArr[k++] = nums1[i++];
+        while (j < n) mergedArr[k++] = nums2[j++];
+
+        int totalLength = m + n;
+        if (totalLength % 2 == 1) {
+            return mergedArr[totalLength / 2];
+        } else {
             return (mergedArr[totalLength / 2 - 1] + mergedArr[totalLength / 2]) / 2.0;
         }
-        
-
-        }
     }
+}
